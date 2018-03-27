48ddd31fed76a08b2b8bbd4d7e35d6879e9c82e5
Unknown <poree47@gmail.com>
2018-03-27 15:16:40 +0200

    Add Header and finish him ! Add button rules + rules !  Add logo

--- hangManCase/src/app/app.module.ts
+++ hangManCase/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
+
 import { HeaderComponent } from './header/header.component';
+import { LogoHeadComponent } from './logo-head/logo-head.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'HangMan')
   ],
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, LogoHeadComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
