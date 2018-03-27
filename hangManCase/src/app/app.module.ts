fddc977fa2c6c0fef8d87f33c18a1f1493ecdbae
Unknown <poree47@gmail.com>
2018-03-27 10:35:28 +0200

    Head

--- hangManCase/src/app/app.module.ts
+++ hangManCase/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'HangMan')
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
