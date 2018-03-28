a7b01ab053c62531e7bb5e4b445e3e7150a78900
BenbachirAzzedine <azzedine.benbachir@gmail.com>
2018-03-28 16:21:44 +0200

    création routing

--- hangManCase/src/app/app.module.ts
+++ hangManCase/src/app/app.module.ts
@@ -3,13 +3,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
+import { ButtonComponent } from './button/button.component';
+import { MatchmakingComponent } from './matchmaking/matchmaking.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'button', component: ButtonComponent },
+  { path: 'matchmaking', component: MatchmakingComponent },
+];
 
 @NgModule({
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AngularFireModule.initializeApp(environment.firebase, 'HangMan')
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, ButtonComponent, MatchmakingComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
